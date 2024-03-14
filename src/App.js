9c9be8147b7a5cfdc056895903f0a3bbd503b0f4
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-15 00:17:21 +0530

    header.jsx wrote

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import React from 'react';
 import './App.css';
+import Header from './components/header/Header'; // Change the import statement to match the casing of the actual file name
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Header/>
+    </div>
   );
 }
 
