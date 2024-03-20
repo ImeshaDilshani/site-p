7d2bcbd399dde2da36ded57268915cb716c8a532
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-20 15:08:34 +0530

    home component started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header'; // Change the import statement to match the casing of the actual file name
+import Home from './components/home/Home'; // Change the import statement to match the casing of the actual file name
 
 function App() {
   return (
     <div>
       <Header/>
+      <main className='main'>
+        <Home/>
+      </main>
     </div>
   );
 }
