596738f1d9764ba8f86287fc440b8fa0f140c8f2
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-21 09:02:23 +0530

    details added

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Social = () => {
   return (
-    <div>Social</div>
+    <div className="home__social">
+        <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+            <li className="uil uil-instagram"></li>
+        </a>
+
+        <a href="https://dribbble.com/" className="home__social-icon" target='_blank'>
+            <li className="uil uil-dribbble"></li>
+        </a>
+
+        <a href="https://github.com/" className="home__social-icon" target='_blank'>
+            <li className="uil uil-github-alt"></li>
+        </a>
+    </div>
   )
 }
 
