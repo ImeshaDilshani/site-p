cd46125c128cb14e9bed0a77377e1aa41138697a
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-23 21:45:56 +0530

    about done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header'; // Change the import statement to match the casing of the actual file name
 import Home from './components/home/Home'; // Change the import statement to match the casing of the actual file name
+import About from './components/about/About';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <main className='main'>
         <Home/>
+        <About/>
       </main>
     </div>
   );
