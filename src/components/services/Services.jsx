9aaa423c813cbcccabe6f5a4da513a5d990901b7
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-25 20:31:17 +0530

    Service section updated

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import './services.css'
 
 const Services = () => {
-    // const [toggleState, setToggleState] = useState();
-    // const toggleTab = (index) => {
-    //     setToggleState(index);
-    // }
+    const [toggleState, setToggleState] = useState();
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
   return (
     <section className="services section" id='services'>
         <h2 className="section__title">Services</h2>
@@ -17,11 +17,11 @@ const Services = () => {
                     <i className="uil uil-web-grid services__icon"></i>
                     <h3 className="services__title">Product <br/> Designer</h3>
                 </div>
-                <span className="services__button">View More
+                <span className="services__button" onClick={()=>toggleTab(1)}>View More
                 <i className="uil uil-arrow-right services__button-icon"></i></span>
-                <div className="services__modal">
+                <div className={toggleState === 1 ? "services__modal active-modal": "services__modal"}>
                     <div className='services__modal-content'>
-                    <i className="uil uil-times services__modal-close"></i>
+                    <i onClick={()=> toggleTab(0)} className="uil uil-times services__modal-close"></i>
                     <h3 className="services__modal-title">Product Designer</h3>
                     <p className="services__modal-description">Service with more than 3 years of experience.Providing quality work to clients and companies.</p>
                     
@@ -50,11 +50,11 @@ const Services = () => {
                     <i className="uil uil-web-grid services__icon"></i>
                     <h3 className="services__title">Product <br/> Designer</h3>
                 </div>
-                <span className="services__button">View More
+                <span className="services__button" onClick={()=>toggleTab(2)}>View More
                 <i className="uil uil-arrow-right services__button-icon"></i></span>
-                <div className="services__modal">
+                <div  className={toggleState === 2 ? "services__modal active-modal": "services__modal"}>
                     <div className='services__modal-content'>
-                    <i className="uil uil-times services__modal-close"></i>
+                    <i onClick={()=> toggleTab(0)} className="uil uil-times services__modal-close"></i>
                     <h3 className="services__modal-title">Product Designer</h3>
                     <p className="services__modal-description">Service with more than 3 years of experience.Providing quality work to clients and companies.</p>
                     
@@ -83,11 +83,11 @@ const Services = () => {
                     <i className="uil uil-web-grid services__icon"></i>
                     <h3 className="services__title">Product <br/> Designer</h3>
                 </div>
-                <span className="services__button">View More
+                <span className="services__button" onClick={()=>toggleTab(3)}>View More
                 <i className="uil uil-arrow-right services__button-icon"></i></span>
-                <div className="services__modal">
+                <div className={toggleState === 3 ? "services__modal active-modal": "services__modal"}>
                     <div className='services__modal-content'>
-                    <i className="uil uil-times services__modal-close"></i>
+                    <i onClick={()=> toggleTab(0)} className="uil uil-times services__modal-close"></i>
                     <h3 className="services__modal-title">Product Designer</h3>
                     <p className="services__modal-description">Service with more than 3 years of experience.Providing quality work to clients and companies.</p>
                     
