7673b7408ca158af5268bf1b49981b8175561860
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-27 18:22:34 +0530

    qualification updated

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './qualification.css'
 
 const Qualification = () => {
+    const [toggleState, setToggleState] = useState(1);
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
   return (
     <section className='qualification section'>
         <h2 className="section__title">Qualification</h2>
@@ -9,11 +14,11 @@ const Qualification = () => {
 
         <div className="qualification__container container">
             <div className="qualification__tabs">
-                <div className="qualification__button button--flex">
+                <div className={toggleState === 1 ? "qualification__button qualification__active button--flex": "qualification__button button--flex"} onClick = {() => toggleTab(1)}>
                     <i className="uil uil-graduation-cap qualification__icon"></i> {""}Education
                 </div>
 
-                <div className="qualification__button button--flex">
+                <div className={toggleState === 2 ? "qualification__button qualification__active button--flex": "qualification__button button--flex"} onClick = {() => toggleTab(2)}>
                     <i className="uil uil-briefcase-alt qualification__icon"></i> {""}Experience
                 </div>
 
@@ -21,13 +26,13 @@ const Qualification = () => {
 
             <div className="qualification__sections">
 
-                <div className="qualification__content qualification__content-active">
+                <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
                     <div className="qualification__data">
                         <div>
                            <h3 className="qualification__title">Web Design</h3>
                            <span className="qualification__subtitle"> Spain - Institute</span>
                             <div className="qualification__calender">
-                               <i className="uil uil-calender-alt">2021 - Present</i>
+                               <i className="uil uil-calendar-alt">2021 - Present</i>
                             </div>
                         </div>
                         
@@ -50,7 +55,7 @@ const Qualification = () => {
                            <h3 className="qualification__title">Art Director</h3>
                            <span className="qualification__subtitle"> Spain - Institute</span>
                             <div className="qualification__calender">
-                               <i className="uil uil-calender-alt">2021 - Present</i>
+                               <i className="uil uil-calendar-alt">2021 - Present</i>
                             </div>
                         </div>
                         
@@ -63,7 +68,7 @@ const Qualification = () => {
                            <h3 className="qualification__title">Web Development</h3>
                            <span className="qualification__subtitle"> Spain - Institute</span>
                             <div className="qualification__calender">
-                               <i className="uil uil-calender-alt">2021 - Present</i>
+                               <i className="uil uil-calendar-alt">2021 - Present</i>
                             </div>
                         </div>
                         
@@ -86,7 +91,7 @@ const Qualification = () => {
                            <h3 className="qualification__title">Art Director</h3>
                            <span className="qualification__subtitle"> Spain - Institute</span>
                             <div className="qualification__calender">
-                               <i className="uil uil-calender-alt">2021 - Present</i>
+                               <i className="uil uil-calendar-alt">2021 - Present</i>
                             </div>
                         </div>
                         
@@ -97,13 +102,13 @@ const Qualification = () => {
                     
                 </div>
 
-                <div className="qualification__content">
+                <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
                     <div className="qualification__data">
                         <div>
                            <h3 className="qualification__title">Web Design</h3>
                            <span className="qualification__subtitle"> Spain - Institute</span>
                             <div className="qualification__calender">
-                               <i className="uil uil-calender-alt">2021 - Present</i>
+                               <i className="uil uil-calendar-alt">2021 - Present</i>
                             </div>
                         </div>
                         
@@ -126,7 +131,7 @@ const Qualification = () => {
                            <h3 className="qualification__title">Art Director</h3>
                            <span className="qualification__subtitle"> Spain - Institute</span>
                             <div className="qualification__calender">
-                               <i className="uil uil-calender-alt">2021 - Present</i>
+                               <i className="uil uil-calendar-alt">2021 - Present</i>
                             </div>
                         </div>
                         
@@ -139,7 +144,7 @@ const Qualification = () => {
                            <h3 className="qualification__title">Web Development</h3>
                            <span className="qualification__subtitle"> Spain - Institute</span>
                             <div className="qualification__calender">
-                               <i className="uil uil-calender-alt">2021 - Present</i>
+                               <i className="uil uil-calendar-alt">2021 - Present</i>
                             </div>
                         </div>
                         
