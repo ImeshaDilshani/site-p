62ace1f8fc54da58aa2ffd616ee906c1840cf0e4
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-29 20:20:37 +0530

    contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,13 +6,40 @@ const Contact = () => {
         <h2 className="section__title">Get in touch</h2>
         <span className="section__subtitle">Contact Me</span>
 
-        <contact__container className="container grid">
+        <div  className="contact__container container grid">
             <div className="contact content">
                 <h3 className="contact__title">Talk to me</h3>
             </div>
 
-            <div className="contact__content"></div>
-        </contact__container>
+            <div className="contact__info">
+                <div className="contact__card">
+                    <i className="bx bx contact__card-icon"></i>
+                    <h3 className="contact__card-title">Email</h3>
+                    <span className="sontact__card-data">user@gmail.com</span>
+                    <a href="" className="contact__button">Write me{""}
+                    <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                    </a>
+                </div>
+
+                <div className="contact__card">
+                    <i className="bx bx contact__card-icon"></i>
+                    <h3 className="contact__card-title"></h3>
+                    <span className="sontact__card-data"></span>
+                    <a href="" className="contact__button">Write me{""}
+                    <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                    </a>
+                </div>
+
+                <div className="contact__card">
+                    <i className="bx bx contact__card-icon"></i>
+                    <h3 className="contact__card-title"></h3>
+                    <span className="sontact__card-data"></span>
+                    <a href="" className="contact__button">Write me{""}
+                    <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
