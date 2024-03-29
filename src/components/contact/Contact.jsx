874353c8490993f390875a95f147da3da5f7e354
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-03-30 00:27:28 +0530

    contact form issue fixed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,33 +10,35 @@ const Contact = () => {
         <div  className="contact__container container grid">
             <div className="contact content">
                 <h3 className="contact__title">Talk to me</h3>
+
                 <div className="contact__info">
-                <div className="contact__card">
-                    <i className="bx bx-mail-send contact__card-icon"></i>
-                    <h3 className="contact__card-title">Email</h3>
-                    <span className="sontact__card-data">user@gmail.com</span>
-                    <a href="" className="contact__button">Write me{""}
-                    <i className="bx bx-right-arrow-alt contact__button-icon"></i>
-                    </a>
-                </div>
 
-                <div className="contact__card">
-                    <i className="bx bxl-whatsapp contact__card-icon"></i>
-                    <h3 className="contact__card-title">Whatsapp</h3>
-                    <span className="sontact__card-data">077-5933501</span>
-                    <a href="" className="contact__button">Write me{""}
-                    <i className="bx bx-right-arrow-alt contact__button-icon"></i>
-                    </a>
-                </div>
+                    <div className="contact__card">
+                        <i className="bx bx-mail-send contact__card-icon"></i>
+                        <h3 className="contact__card-title">Email</h3>
+                        <span className="contact__card-data">user@gmail.com</span>
+                        <a href="" className="contact__button">Write me{""}
+                            <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                        </a>
+                    </div>
 
-                <div className="contact__card">
-                    <i className="bx bxl-messenger contact__card-icon"></i>
-                    <h3 className="contact__card-title">Messenger</h3>
-                    <span className="sontact__card-data">user.fb123</span>
-                    <a href="" className="contact__button">Write me{""}
-                    <i className="bx bx-right-arrow-alt contact__button-icon"></i>
-                    </a>
-                </div>
+                    <div className="contact__card">
+                        <i className="bx bxl-whatsapp contact__card-icon"></i>
+                        <h3 className="contact__card-title">Whatsapp</h3>
+                        <span className="contact__card-data">077-5933501</span>
+                        <a href="" className="contact__button">Write me{""}
+                            <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                        </a>
+                    </div>
+
+                    <div className="contact__card">
+                        <i className="bx bxl-messenger contact__card-icon"></i>
+                        <h3 className="contact__card-title">Messenger</h3>
+                        <span className="contact__card-data">user.fb123</span>
+                        <a href="" className="contact__button">Write me{""}
+                            <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                        </a>
+                    </div>
             </div>
             </div>
 
@@ -67,6 +69,7 @@ const Contact = () => {
                         <label htmlFor="" className="contact__form-tag">Project</label>
                         <textarea name="project" id="" cols="30" rows="10" className='contact__form-input' placeholder='Write your project'></textarea>
                     </div>
+
                     <button  className="button button--flex">
                         Send Message
                             <svg
@@ -87,6 +90,7 @@ const Contact = () => {
                                 ></path>
                                 </svg>
                     </button>
+
                 </form>
             </div>
         </div>
