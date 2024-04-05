ce0ceb0ca91cdebbbf7e24dd0d8dcd3e16ae1a91
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-04-05 22:32:25 +0530

    scrollup updated

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import  './scrollup.css'
 
 const ScrollUp = () => {
+  window.addEventListener('scroll', function(){
+    const scrollUp = this.document.querySelector(".scrollup");
+    if(this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  })  
   return (
     <a href="" className="scrollup">
         <i className="uil uil-arrow-up scrollup__icon"></i>
