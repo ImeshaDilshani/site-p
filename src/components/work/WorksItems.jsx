371d3920f73528ccac403ed73fa6bddf3fb6d7e2
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-04-30 20:24:37 +0530

    updated

--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -6,9 +6,12 @@ const WorksItems = ({item}) => {
     <div className="work__card" key={item.id}>
         <img src={item.image} alt="" className='work__img'/>
         <h3 className="work__title">{item.title}</h3>
-        <a href="#" className="work__button">
+        {/* <a href="#" className="work__button">
             Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
-        </a>
+        </a> */}
+        <a href={item.githubLink} target="_blank" rel="noopener noreferrer" className="work__button">
+        GitHub <i className="bx bxl-github work__button-icon"></i>
+      </a>
     </div>
   )
 }
