e2a8bc2e81189a2c0252f1d1b96a8445d1540827
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-05-01 17:18:42 +0530

    skill updated

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -52,7 +52,7 @@ const Frontend = () => {
                     <i className="bx bx-badge-check"></i>
 
                     <div>
-                        <h3 className="skills__name">Git</h3>
+                        <h3 className="skills__name">Flutter</h3>
                         <span className="skills__level">Intermediate</span>
                     </div>
                     
