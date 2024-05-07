e182e391afe786fc440eaf83472c2ce01ac12e84
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-05-07 17:22:47 +0530

    simple updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -40,11 +40,11 @@ const Header = () => {
                         </a>
                     </li>
 
-                    <li className="nav__item">
+                    {/* <li className="nav__item">
                         <a href="#contact" className="nav__link">
                             <i className="uil uil-message nav__icon"></i>Contact
                         </a>
-                    </li>
+                    </li> */}
                 </ul>
                 <i className="uil uil-times nav__close" onClick={()=>showMenu(!Toggle)}></i>
             </div>
